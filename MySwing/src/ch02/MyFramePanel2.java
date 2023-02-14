feafbca9b00f2144e4de9242fcababdf2b2e60c9
afeelingeffort <kmg11519@google.com>
2023-02-14 11:59:05 +0900

    수정

--- MySwing/src/ch02/MyFramePanel2.java
+++ MySwing/src/ch02/MyFramePanel2.java
@@ -10,12 +10,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MyFramePanel2 extends JFrame {
-	private JButton button1;
-	private JButton button2;
-	private JButton button3;
-	private JButton button4;
-	private JButton button5;
-	private JButton button6;
+//	private JButton button1;
+//	private JButton button2;
+//	private JButton button3;
+//	private JButton button4;
+//	private JButton button5;
+//	private JButton button6;
+	
+	private JButton[] buttons = new JButton[6];
 
 	// 패널 : 컴포넌트들을 그룹화 시킬 수 있다. 즉 각각의 배체관리자를 지정할 수 있다.
 	private JPanel panel1;
@@ -36,12 +38,19 @@ public class MyFramePanel2 extends JFrame {
 		panel2.setBackground(Color.yellow);
 
 		// 버튼 초기화
-		button1 = new JButton("button1");
-		button2 = new JButton("button2");
-		button3 = new JButton("button3");
-		button4 = new JButton("button4");
-		button5 = new JButton("button5");
-		button6 = new JButton("button6");
+		for (int i = 0; i < 3; i++) {
+			buttons[i] = new JButton("button" + (i + 1));
+		}
+
+		for (int i = 3; i < 6; i++) {
+			buttons[i] = new JButton("button" + (i + 1));
+		}
+//		button1 = new JButton("button1");
+//		button2 = new JButton("button2");
+//		button3 = new JButton("button3");
+//		button4 = new JButton("button4");
+//		button5 = new JButton("button5");
+//		button6 = new JButton("button6");
 	}
 
 	private void setInitLayout() {
@@ -51,16 +60,22 @@ public class MyFramePanel2 extends JFrame {
 		panel2.setLayout(new FlowLayout(FlowLayout.RIGHT));
 
 		// 루트 패널 기본 레이아웃 BorderLayout이다.
-		// 하지만 추가적으로 만들어 사용하는 Panel은 기본 레이아웃이 FlowLayout이다.
-		panel1.add(button1);
-		panel1.add(button2);
-		panel1.add(button3);
+		// 하지만 추가적으로 만들어 사용하는 Panel은 기본 레이아웃이 FloLayout이다.
+		for (int i = 0; i < 3; i++) {
+			panel1.add(buttons[i]);
+		}
+//		panel1.add(button1);
+//		panel1.add(button2);
+//		panel1.add(button3);
 		// panel1.setLayout(null);
 
-		panel2.add(button4);
-		panel2.add(button5);
-		panel2.add(button6);
-		
+		for (int i = 3; i < 6; i++) {
+			panel2.add(buttons[i]);
+		}
+//		panel2.add(button4);
+//		panel2.add(button5);
+//		panel2.add(button6);
+
 		setVisible(true);
 	}
 
