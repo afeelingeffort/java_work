705a06ce8fa28119907b46d6fa840bacb87bf392
afeelingeffort <kmg11519@google.com>
2023-02-20 15:25:35 +0900

    backtroundPlayerService 생성 및 보정

--- bubble/src/ch01/BubbleFrame.java
+++ bubble/src/ch01/BubbleFrame.java
@@ -7,11 +7,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class BubbleFrame extends JFrame{
+public class BubbleFrame extends JFrame {
 
 	private JLabel backgroundMap;
 	private Player player;
-	
+
 	public BubbleFrame() {
 		initData();
 		setInitLayout();
@@ -19,10 +19,11 @@ public class BubbleFrame extends JFrame{
 	}
 
 	private void initData() {
-		backgroundMap=new JLabel(new ImageIcon("images/backgroundMap.png"));
+		backgroundMap = new JLabel(new ImageIcon("images/backgroundMap.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(backgroundMap);
 		setSize(1000, 640);
-		
+
 		player = new Player();
 	}
 
@@ -30,9 +31,9 @@ public class BubbleFrame extends JFrame{
 		setLayout(null);
 		setResizable(false);
 		setLocationRelativeTo(null);
-		
+
 		add(player);
-		
+
 		setVisible(true);
 	}
 
@@ -40,11 +41,25 @@ public class BubbleFrame extends JFrame{
 		this.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				switch(e.getKeyCode()) {
-				case KeyEvent.VK_RIGHT : 
-					
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_RIGHT:
+					player.right();
+					break;
+				case KeyEvent.VK_LEFT:
+					player.left();
+					break;
+				case KeyEvent.VK_UP:
+					player.up();
+					break;
+				case KeyEvent.VK_DOWN:
+					player.down();
+					break;
 				}
 			}
 		});
+	}
+	
+	public static void main(String[] args) {
+		new BubbleFrame();
 	}
 }
--- bubble/src/ch01/Player.java
+++ bubble/src/ch01/Player.java
@@ -3,12 +3,20 @@ package ch01;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class Player extends JLabel implements Moveable{
-	
+public class Player extends JLabel implements Moveable {
+
 	private int x;
 	private int y;
 	private ImageIcon playerR, playerL;
-	
+
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+
+	private final int SPEED = 4;
+	private final int JUMPSPEED = 2;
+
 	public Player() {
 		initData();
 		setInitLayout();
@@ -17,13 +25,18 @@ public class Player extends JLabel implements Moveable{
 	private void initData() {
 		playerR = new ImageIcon("images/playerR.png");
 		playerL = new ImageIcon("images/playerL.png");
+
+		left = false;
+		right = false;
+		up = false;
+		down = false;
 	}
 
-	//기본값 세팅
+	// 기본값 세팅
 	private void setInitLayout() {
 		x = 55;
 		y = 535;
-		
+
 		setSize(50, 50);
 		setLocation(x, y);
 		setIcon(playerR);
@@ -31,25 +44,68 @@ public class Player extends JLabel implements Moveable{
 
 	@Override
 	public void left() {
-		setIcon(playerL);
-		x-=10;
-		setLocation(x, y);
+		left = true;
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while (left) {
+					setIcon(playerL);
+					x -= SPEED;
+					setLocation(x, y);
+
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				} // end of while
+			}
+		}).start();
+
 	}
 
 	@Override
 	public void right() {
-		
+		right = true;
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while (right) {
+					setIcon(playerR);
+					x += SPEED;
+					setLocation(x, y);
+					
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+
 	}
 
 	@Override
 	public void up() {
+		up = true;
 		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+			}
+		}).start();
 	}
 
 	@Override
 	public void down() {
-		
+		System.out.println("player down");
 	}
-	
-	
+
 }
